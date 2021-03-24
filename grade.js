bdc9f4b010ffc1d892c974dce8a36bd2a1b2cce4
Watotacho <watotacho@gmail.com>
2021-03-24 07:38:29 +0300

    first commit

---
+++ grade.js
@@ -0,0 +1,29 @@
+function getGrade(marks){
+
+if(marks>=84){
+    grade='A';
+ console.log(grade);
+}else if(marks>=70){
+    grade='B';
+    console.log(grade);
+}else if(marks>=69){
+    grade='C';
+    console.log(grade);
+
+}else if(marks>=59){
+    grade='D';
+    console.log(grade);
+
+}else if(marks>=0){
+    grade='E';
+    console.log(grade);
+
+
+}else{
+    grade='F';
+    console.log(grade);
+}
+
+
+}
+getGrade(86);
